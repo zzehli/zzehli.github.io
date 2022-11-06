7eb18972df18503310a08cc3a39273c755b1bf95
jae <albertforus@gmail.com>
2022-11-05 21:56:24 -0400

    composition and content draft

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],  
+  theme: {
+    extend: {
+      screens: {
+        "sm": "480px"
+      }
+    },
+    fontFamily:{
+      ubuntu: ['Rubik', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
+// https://coolors.co/fe938c-e6b89c-ead2ac-9cafb7-4281a4
